3d17c12c977152ec979b36f397d7a01d256ce911
veniamin <wavybentley@gmail.com>
2026-07-06 17:19:28 +0200

    updated favi coin

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,16 +24,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/images/noun-lips-33267-removebg-preview.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/images/noun-lips-33267-removebg-preview.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/images/noun-lips-33267-removebg-preview.png',
+        type: 'image/png',
       },
     ],
     apple: '/apple-icon.png',
